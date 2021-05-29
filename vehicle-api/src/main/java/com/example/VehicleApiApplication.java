da8cb7af5fc41e26fe01641151960241d7879a5c
matsu-chara <matsuy00@gmail.com>
2021-05-29 10:52:59 +0900

    nativeHint

--- vehicle-api/src/main/java/com/example/VehicleApiApplication.java
+++ vehicle-api/src/main/java/com/example/VehicleApiApplication.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.NativeHint;
 
 @SpringBootApplication
+@NativeHint(options = {"--enable-all-security-services" })
 public class VehicleApiApplication {
 
 	public static void main(String[] args) {
